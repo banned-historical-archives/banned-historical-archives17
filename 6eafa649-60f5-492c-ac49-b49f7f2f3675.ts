392b93b4abaee6553f2ac17bf8d7154d74c5bb7a
Your Name <you@example.com>
2024-01-11 07:27:39 +0000

    feat: [自动化文稿录入]文革通讯第30、31、32期

---
+++ 6eafa649-60f5-492c-ac49-b49f7f2f3675.ts
@@ -0,0 +1,19 @@
+export default {
+  resource_type: 'book',
+  entity: {
+    id: '6eafa649-60f5-492c-ac49-b49f7f2f3675',
+    name: '文革通讯第30、31、32期',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '浙江工农兵美术大学红卫兵战斗队、毛泽东主义红卫兵',
+    files: ["'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/xx.pdf"]
+  },
+  parser_option: {
+    articles: [{"title":"毛主席最新指示","authors":["毛泽东"],"page_start":1,"page_end":1,"dates":[{"year":1967,"month":5,"day":1}]},{"title":"中央首长讲话","authors":["江青","戚本禹"],"page_start":1,"page_end":2,"dates":[{"year":1967,"month":5}]},{"title":"北京市革命委员会重要通告","authors":["北京市革命委员会"],"page_start":2,"page_end":2,"dates":[{"year":1967,"month":5,"day":14}]},{"title":"上海市革委会转中央文革办事处","authors":["江青","戚本禹"],"page_start":2,"page_end":3,"dates":[{"year":1967,"month":5}]},{"title":"祖国各地","authors":["文革通讯编辑部"],"page_start":3,"page_end":5,"dates":[{"year":1967,"month":5,"day":17}]},{"title":"北京来电","authors":["文革通讯编辑部"],"page_start":5,"page_end":6,"dates":[{"year":1967,"month":5,"day":17}]},{"title":"黑《修养》反毛泽东思想罪行一百例","authors":["文革通讯编辑部"],"page_start":7,"page_end":21,"dates":[{"year":1967,"month":5,"day":17}]},{"title":"刘少奇十七年来在文艺方面的反动言论","authors":["文革通讯编辑部"],"page_start":21,"page_end":24,"dates":[{"year":1949},{"year":1966}]}],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation'
+      }',
+};
