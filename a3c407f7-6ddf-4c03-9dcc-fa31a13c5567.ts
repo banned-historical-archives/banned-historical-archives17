ca2fdb453880a3760e32fc4ebb612491c84d4a4b
Your Name <you@example.com>
2024-08-10 22:02:45 +0800

    1

---
+++ a3c407f7-6ddf-4c03-9dcc-fa31a13c5567.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "a3c407f7-6ddf-4c03-9dcc-fa31a13c5567",
+    "name": "彻底批判“四人邦”及其黑手王体、王弼现之流，制造“八·二三”反革命打砸抢事件，残酷迫害革命老干部的滔天罪行",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "任德忠",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/a3c407f7-6ddf-4c03-9dcc-fa31a13c5567/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/a3c407f7-6ddf-4c03-9dcc-fa31a13c5567/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/a3c407f7-6ddf-4c03-9dcc-fa31a13c5567/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/a3c407f7-6ddf-4c03-9dcc-fa31a13c5567/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "彻底批判“四人邦”及其黑手王体、王弼现之流，制造“八·二三”反革命打砸抢事件，残酷迫害革命老干部的滔天罪行",
+        "authors": [
+          "任德忠"
+        ],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1977,
+            "month": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a3c407f7-6ddf-4c03-9dcc-fa31a13c5567",
+  "resource_type": "book",
+  "version": 2
+}
