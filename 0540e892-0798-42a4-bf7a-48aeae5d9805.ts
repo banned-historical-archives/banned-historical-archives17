ee12296025cad3bb04ff792acd6f1aaa493fae0a
Your Name <you@example.com>
2024-08-13 20:25:42 +0800

    1

--- 0540e892-0798-42a4-bf7a-48aeae5d9805.ts
+++ 0540e892-0798-42a4-bf7a-48aeae5d9805.ts
@@ -1,47 +1,47 @@
 export default {
-  "entity": {
-    "id": "0540e892-0798-42a4-bf7a-48aeae5d9805",
-    "name": "彻底揭发批判“四人帮”伸向山西的黑手段立生的反革命滔天罪行",
-    "internal": false,
-    "type": "imgs",
-    "official": false,
-    "author": "武爱全",
-    "files": [
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/1.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/2.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/3.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/4.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/5.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/6.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/7.jpg"
-    ]
-  },
-  "parser_option": {
-    "articles": [
-      {
-        "title": "彻底揭发批判“四人帮”伸向山西的黑手段立生的反革命滔天罪行",
-        "authors": [
-          "武爱全"
-        ],
-        "page_start": 1,
-        "page_end": 7,
-        "dates": [
-          {
-            "year": 1977,
-            "month": 12,
-            "day": 26
-          }
-        ]
-      }
+  entity: {
+    id: '0540e892-0798-42a4-bf7a-48aeae5d9805',
+    name: '彻底揭发批判“四人帮”伸向山西的黑手段立生的反革命滔天罪行',
+    internal: false,
+    type: 'img',
+    official: false,
+    author: '武爱全',
+    files: [
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/1.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/2.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/3.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/4.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/5.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/6.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/0540e892-0798-42a4-bf7a-48aeae5d9805/7.jpg',
     ],
-    "ocr": {
-      "use_onnx": true,
-      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
-      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
-    }
   },
-  "parser_id": "automation",
-  "path": "0540e892-0798-42a4-bf7a-48aeae5d9805",
-  "resource_type": "book",
-  "version": 2
-}
+  parser_option: {
+    articles: [
+      {
+        title: '彻底揭发批判“四人帮”伸向山西的黑手段立生的反革命滔天罪行',
+        authors: [
+          '武爱全',
+        ],
+        page_start: 1,
+        page_end: 7,
+        dates: [
+          {
+            year: 1977,
+            month: 12,
+            day: 26,
+          },
+        ],
+      },
+    ],
+    ocr: {
+      use_onnx: true,
+      det_model_dir: './paddle/onnx/ch_PP-OCRv4_det_infer.onnx',
+      rec_model_dir: './paddle/onnx/ch_PP-OCRv4_rec_infer.onnx',
+    },
+  },
+  parser_id: 'automation',
+  path: '0540e892-0798-42a4-bf7a-48aeae5d9805',
+  resource_type: 'book',
+  version: 2,
+};
--- 39e8e48b-dcf4-47f8-9cfc-42f793d8bf58.ts
+++ 39e8e48b-dcf4-47f8-9cfc-42f793d8bf58.ts
@@ -1,52 +1,52 @@
 export default {
-  "entity": {
-    "id": "39e8e48b-dcf4-47f8-9cfc-42f793d8bf58",
-    "name": "“四人帮”死党毛远新在鞍山的主要罪行",
-    "internal": false,
-    "type": "imgs",
-    "official": false,
-    "author": "中共鞍山市委清查王张江姚反党集团罪行小组办公室",
-    "files": [
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/1.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/2.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/3.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/4.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/5.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/6.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/7.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/8.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/9.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/10.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/11.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/12.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/13.jpg"
-    ]
-  },
-  "parser_option": {
-    "articles": [
-      {
-        "title": "“四人帮”死党毛远新在鞍山的主要罪行",
-        "authors": [
-          "中共鞍山市委清查王张江姚反党集团罪行小组办公室"
-        ],
-        "page_start": 1,
-        "page_end": 13,
-        "dates": [
-          {
-            "year": 1977,
-            "month": 1
-          }
-        ]
-      }
+  entity: {
+    id: '39e8e48b-dcf4-47f8-9cfc-42f793d8bf58',
+    name: '“四人帮”死党毛远新在鞍山的主要罪行',
+    internal: false,
+    type: 'img',
+    official: false,
+    author: '中共鞍山市委清查王张江姚反党集团罪行小组办公室',
+    files: [
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/1.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/2.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/3.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/4.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/5.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/6.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/7.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/8.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/9.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/10.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/11.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/12.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives17/main/39e8e48b-dcf4-47f8-9cfc-42f793d8bf58/13.jpg',
     ],
-    "ocr": {
-      "use_onnx": true,
-      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
-      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
-    }
   },
-  "parser_id": "automation",
-  "path": "39e8e48b-dcf4-47f8-9cfc-42f793d8bf58",
-  "resource_type": "book",
-  "version": 2
-}
+  parser_option: {
+    articles: [
+      {
+        title: '“四人帮”死党毛远新在鞍山的主要罪行',
+        authors: [
+          '中共鞍山市委清查王张江姚反党集团罪行小组办公室',
+        ],
+        page_start: 1,
+        page_end: 13,
+        dates: [
+          {
+            year: 1977,
+            month: 1,
+          },
+        ],
+      },
+    ],
+    ocr: {
+      use_onnx: true,
+      det_model_dir: './paddle/onnx/ch_PP-OCRv4_det_infer.onnx',
+      rec_model_dir: './paddle/onnx/ch_PP-OCRv4_rec_infer.onnx',
+    },
+  },
+  parser_id: 'automation',
+  path: '39e8e48b-dcf4-47f8-9cfc-42f793d8bf58',
+  resource_type: 'book',
+  version: 2,
+};
